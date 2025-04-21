2987fc839d278255e7d91f59234a9c135089c46b
Dimon159 <92216038+Vas34hs@users.noreply.github.com>
2025-04-21 15:56:55 +0500

    Домашка

---
+++ Домашнее задание - python 1.py
@@ -0,0 +1,56 @@
+
+
+
+# Печать по строкам
+# Задание 1
+
+# print("Nothing")
+# print("will work")
+# print("unless you do")
+
+
+# Задание 2
+
+# print('“Anyone who')
+# print('    stops')
+# print('       learning is old,')
+# print('          whether at twenty or eighty”.')
+# print('                                Henry Ford')
+
+
+# задание 3
+#
+# a = float(input("первое число: "))
+# b = float(input("  второе число: "))
+#
+# # Вычисления
+# sum = a + b
+# minus = a - b
+# umnojenie = a * b
+#
+# print("Сумма:", sum)
+# print("Разность:", minus)
+# print("Умножение:", umnojenie)
+
+# задание 4
+
+# chislo = float(input("число: "))
+# procent = float(input("процент: "))
+# result  = chislo5 * procent / 100
+# print(f"{procent} процентов от {chislo} = {result}")
+
+# задание 5
+
+width = float(input("ширинf прямоугольника: "))
+height = float(input("высота прямоугольника :"))
+area = width * height
+print("          🔎 Площадь прямоугольникf:   ", area)
+
+
+
+
+
+
+# print("   Anyone who")
+# print("stops")
+
